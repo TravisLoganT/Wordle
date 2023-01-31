62c0c5f29cfd825e8018a78853bc50a7f7d41a40
Travis Logan <c1014427@newcastle.ac.uk>
2023-01-31 22:47:30 +0000

    bugfix: Capitilisation consistency

--- Wordle.py
+++ Wordle.py
@@ -1,5 +1,5 @@
 # Get a chosen word from the user
-guess = input("Guess a word: ")
+guess = input("Guess a word: ").upper()
 
 # Display if the user has successfully guessed the word
 if guess == "PEPSI":
