5c3d6c5a755a0f6aa12c47f18d3d4dd15ac15250
Travis Logan <c1014427@newcastle.ac.uk>
2023-01-31 22:38:09 +0000

    Created a very basic word guessing program

---
+++ Wordle.py
@@ -0,0 +1,8 @@
+# Get a chosen word from the user
+guess = input("Guess a word: ")
+
+# Display if the user has successfully guessed the word
+if guess == "PEPSI":
+    print("Correct")
+else:
+    print("Wrong")
