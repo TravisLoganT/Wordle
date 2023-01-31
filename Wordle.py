c7cc59abcdd631b9af3dd46fcbd6611eba5a7b1a
Travis Logan <c1014427@newcastle.ac.uk>
2023-01-31 22:59:02 +0000

    Created a total number of allowes tries

--- Wordle.py
+++ Wordle.py
@@ -1,8 +1,10 @@
-# Get a chosen word from the user
-guess = input("Guess a word: ").upper()
+for guess_number in range(1, 7):
+    # Get a chosen word from the user
+    guess = input(f"Guess {guess_number}: ").upper() 
 
-# Display if the user has successfully guessed the word
-if guess == "PEPSI":
-    print("Correct")
-else:
+    # Display if the user has successfully guessed the word
+    if guess == "PEPSI":
+        print("Correct")
+        break
+    
     print("Wrong")
