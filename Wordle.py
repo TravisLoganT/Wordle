ed086c87b83e0f29be2d12406b9e676745f5511f
Travis Logan <c1014427@newcastle.ac.uk>
2023-01-31 23:25:57 +0000

    Created a better output for what letters are correct

--- Wordle.py
+++ Wordle.py
@@ -1,10 +1,28 @@
+WORD = "BLACK"
+
 for guess_number in range(1, 7):
     # Get a chosen word from the user
     guess = input(f"Guess {guess_number}: ").upper() 
 
     # Display if the user has successfully guessed the word
-    if guess == "PEPSI":
+    if guess == WORD:
         print("Correct")
         break
     
-    print("Wrong")
+    # check each letter and if it's the right letter and in correct spot,
+    # put into a list
+    correct_letters = {
+        letter for letter, correct in zip(guess, WORD) if letter == correct
+    }
+
+    # put letters that are correct put not in the correct position into a list
+    misplaced_letters = set(guess) & set(WORD) - correct_letters
+
+    # state the letters that are not in the word at all into a list
+    wrong_letters = set(guess) - set(WORD)
+
+    # Show the user the information about their guess
+    print("Correct letters:", ", ".join(sorted(correct_letters)))
+    print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
+    print("Incorrect letters:", ", ".join(sorted(wrong_letters)))
+
