e7273c6c159859d2e5f5e69513ddee962a911ac0
Travis Logan <c1014427@newcastle.ac.uk>
2023-02-01 12:48:42 +0000

    Added functions to allow for future styling

--- Wordle.py
+++ Wordle.py
@@ -1,24 +1,25 @@
 import pathlib, random
+from string import ascii_letters
 
-# acquire the list of words
-WORDLIST = pathlib.Path("wordlist.txt")
 
-# sort the file and grab each word on a new line and then choose a random word
-words = [
-    word.upper()
-    for word in WORDLIST.read_text(encoding="utf-8").strip().split("\n")
-]
-word = random.choice(words)
+def get_random_word():
+    """Choose a random word from the wordist that has been generated"""
 
-for guess_number in range(1, 7):
-    # Get a chosen word from the user
-    guess = input(f"Guess {guess_number}: ").upper() 
+    # acquire the list of words
+    wordlist = pathlib.Path("wordlist.txt")
 
-    # Display if the user has successfully guessed the word
-    if guess == word:
-        print("Correct")
-        break
-    
+    # sort the file and grab each word on a new line and then choose a random word
+    words = [
+        word.upper()
+        for word in wordlist.read_text(encoding="utf-8").strip().split("\n")
+        if len(word) == 5 and all(letter in ascii_letters for letter in word)
+    ]
+    return random.choice(words)
+
+
+def show_guess(guess, word):
+    """Compare the users inputted word to the randomly chosen word"""
+
     # check each letter and if it's the right letter and in correct spot,
     # put into a list
     correct_letters = {
@@ -35,25 +36,32 @@ for guess_number in range(1, 7):
     print("Correct letters:", ", ".join(sorted(correct_letters)))
     print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
     print("Incorrect letters:", ", ".join(sorted(wrong_letters)))
-else:
+
+
+def game_over(word):
+    """Display what the word the user was trying to guess"""
+
     print(f"The word was {word}")
+
 
 
 def main():
     # This will start the pre-process of the game of getting a word
-    # word = get_random_word(...)
+    word = get_random_word()
 
     #This will be the main process of the game of actually playing
     for guess_number in range(1, 7):
         guess = input(f"Guess {guess_number}: ").upper()
 
-        # show_guess(...)
+        show_guess(guess, word)
         if guess == word:
             break
 
 
     # This will the post process of the game, displaying the result
-    # else:
-        # game_over(...)
+    else:
+        game_over(word)
 
 
+if __name__ == "__main__":
+    main()
